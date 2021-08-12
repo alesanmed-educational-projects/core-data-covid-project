6506a19c6b2e5e470d3d07588a6b166db2e3ce8f
Ale Sanchez <alesanchezmedina@gmail.com>
2021-08-12 17:15:01 +0200

    feat: Added endpoint to create countries

--- backend/covid-backend/countries.py
+++ backend/covid-backend/countries.py
@@ -1,16 +1,24 @@
 import json
+from http import HTTPStatus
+from logging import getLogger
 
-from covid_data.db.queries import get_all_countries, get_provinces_by_country
+from covid_data.db.queries import (
+    create_country,
+    get_all_countries,
+    get_provinces_by_country,
+)
 from flask import Blueprint, request
 from flask.wrappers import Response
+from werkzeug import exceptions
 
+from .auth import authenticated
 from .db import get_db
 from .utils import serialize_json
 
 bp = Blueprint("countries", __name__, url_prefix="/countries")
 
 
-@bp.route("/", methods=["GET"])
+@bp.route("", methods=["GET"])
 def get_countries():
     db = get_db()
 
@@ -24,16 +32,38 @@ def get_countries():
             near_fixed = [float(p) for p in near.split(",")]
 
             if len(near_fixed) != 2:
-                raise ValueError()
+                raise exceptions.BadRequest()
 
-        except ValueError:
-            raise ValueError(f"Lat long field {near} not valid")
+        except exceptions.BadRequest:
+            raise exceptions.BadRequest(f"Lat long field {near} not valid")
 
     contries = get_all_countries(db, name, near_fixed)
 
     return Response(
         json.dumps(contries, default=serialize_json),
-        200,
+        HTTPStatus.OK,
+        {"content-type": "application/json"},
+    )
+
+
+@bp.route("", methods=["POST"])
+@authenticated
+def add_country():
+    logger = getLogger("covid-backend")
+
+    db = get_db()
+
+    country_data = request.json or {}
+
+    try:
+        inserted_id = create_country(country_data, db)
+    except Exception as e:
+        logger.error(e)
+        raise exceptions.BadRequest("Invalid country")
+
+    return Response(
+        json.dumps({"id": inserted_id}, default=serialize_json),
+        HTTPStatus.CREATED,
         {"content-type": "application/json"},
     )
 
@@ -46,6 +76,6 @@ def get_provinces(country_id: int):
 
     return Response(
         json.dumps(provinces, default=serialize_json),
-        200,
+        HTTPStatus.OK,
         {"content-type": "application/json"},
     )
