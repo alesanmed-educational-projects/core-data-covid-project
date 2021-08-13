02cc717bef3dba0ef0e78d82a10a85c928e31588
Ale Sanchez <alesanchezmedina@gmail.com>
2021-08-13 08:14:39 +0200

    feat: Removed data laoder in favor of a git submodule

--- data-loader/covid_data/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-import re
-
-with open(os.path.join(os.path.dirname(__file__), "../", "pyproject.toml"), "r") as f:
-    project_info = f.read()
-
-    regex = r"version = \"(.*)\""
-
-    __version__ = re.findall(regex, project_info)[0]
--- data-loader/covid_data/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .components_hierarchy import COMPONENTS_HIERARCHY
-from .components_mapping import COMPONENTS_MAPPING
-
-__all__ = ["COMPONENTS_MAPPING", "COMPONENTS_HIERARCHY"]
